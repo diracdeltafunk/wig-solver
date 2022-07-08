2ce3a6b54d96bf5762e362e58b692d94009040e1
Ben Spitz <diracdeltafunk@users.noreply.github.com>
2022-07-07 20:35:15 -0700

    Rename solve to solveStrict, prepping for new algs

--- solver/index.py
+++ solver/index.py
@@ -14,7 +14,7 @@ def hello_world():
 
 @app.route("/solve", methods=['POST'])
 def run_solver():
-    return jsonify(solver.solve({k: set(v) for d in request.get_json() for (k, v) in d.items()}))
+    return jsonify(solver.solveStrict({k: set(v) for d in request.get_json() for (k, v) in d.items()}))
 
 
 if __name__ == "__main__":
--- solver/solver.py
+++ solver/solver.py
@@ -4,8 +4,10 @@ from scipy.optimize import milp
 from itertools import chain
 import numpy as np
 
+# Standard set-cover solve. Find a minimum collection of sets which cover the universe.
 
-def solve(sets):
+
+def solveStrict(sets):
     num_sets = len(sets.keys())
     # universe is the union of the sets
     universe = set(chain(*sets.values()))
@@ -25,3 +27,25 @@ def solve(sets):
                     l, u], constraints=[A, b_l, b_u]).x
     ids_to_use = [k for (i, k) in enumerate(sets.keys()) if solution[i] == 1]
     return ids_to_use
+
+# Relaxed set-cover solve. Find a minimum collection of sets which cover the universe, except at most k elements.
+# def solveRelaxed(sets, k):
+#     num_sets = len(sets.keys())
+#     # universe is the union of the sets
+#     universe = set(chain(*sets.values()))
+#     num_elems = len(universe)
+#     # Minimize the number of sets
+#     c = np.ones(num_sets)
+#     # All variables are binary integers
+#     integrality = np.ones_like(c)
+#     l = np.zeros_like(c)
+#     u = np.ones_like(c)
+#     # Subject to the constraint that each element appears at least once
+#     A = np.array([[1 if e in s else 0 for s in sets.values()]
+#                  for e in universe])
+#     b_l = np.ones(num_elems)
+#     b_u = np.full_like(b_l, np.inf)
+#     solution = milp(c=c, integrality=integrality, bounds=[
+#                     l, u], constraints=[A, b_l, b_u]).x
+#     ids_to_use = [k for (i, k) in enumerate(sets.keys()) if solution[i] == 1]
+#     return ids_to_use
